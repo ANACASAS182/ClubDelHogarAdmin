9976dc2054fa9b7eff3c3c4ed83e7c2f27b9d448
AbrahamCasasSistemasPerseo <abraham.casas@sistemasperseo.com>
2025-05-02 09:18:10 -0600

    x
    x

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Esto asegura que escanea tus componentes y templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
